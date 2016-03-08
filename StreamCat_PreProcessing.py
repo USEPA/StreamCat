0895b68883489f67eda5d03d2af36339e9772027
Marc Weber <mweber36@gmail.com>
2016-03-07 17:12:50 -0800

    Adding control tables

--- StreamCat_PreProcessing.py
+++ StreamCat_PreProcessing.py
@@ -15,28 +15,22 @@
 import pandas as pd
 from datetime import datetime as dt
 import sys, os
-import numpy as np
 sys.path.append('F:/Watershed Integrity Spatial Prediction/Scripts')
 from StreamCat_functions import Reclass, rasterMath, getRasterInfo, dbf2DF, rat_to_dict
-from osgeo import gdal, gdalconst
-import geopandas as gpd
-import fiona
-from fiona.crs import from_epsg    
+import geopandas as gpd   
 from subprocess import call
-from rasterio.rio import warp
 import arcpy
 #############################
 # Parameters
-#ControlTable  = pd.read_csv(sys.argv[1])
-ControlTable = pd.read_csv('L:/Priv/CORFiles/Geospatial_Library/Data/Project/SSWR1.1B/ControlTables/RasterControlTable.csv')
+ControlTable  = pd.read_csv(sys.argv[1])
+#ControlTable = pd.read_csv('L:/Priv/CORFiles/Geospatial_Library/Data/Project/SSWR1.1B/ControlTables/RasterControlTable.csv')
 ReClassTable = pd.read_csv('L:/Priv/CORFiles/Geospatial_Library/Data/Project/SSWR1.1B/ControlTables/ReclassTable.csv')
 FieldCalcTable =pd.read_csv('L:/Priv/CORFiles/Geospatial_Library/Data/Project/SSWR1.1B/ControlTables/FieldCalcTable.csv')
 #####################################################################################################################
 # Populate variables from control table
 InDir = ControlTable.DirectoryLocations[0]
-Template = ControlTable.DirectoryLocations[1]
-TempDir = ControlTable.DirectoryLocations[2]
-FinalDir = ControlTable.DirectoryLocations[3]
+TempDir = ControlTable.DirectoryLocations[1]
+FinalDir = ControlTable.DirectoryLocations[2]
 #####################################################################################################################
 
 for line in ControlTable.values: # loop through each landscape_var in control table 
@@ -53,6 +47,8 @@ for line in ControlTable.values: # loop through each landscape_var in control ta
         Convert = line[10]
         ConvertFields = line[11]
         ConvertRes = int(line[12])
+        UseArcpy = line[13]
+        UseStatesMask = line[14]
         if FileType!= 'ESRI Shapefile':
             #get raster info for input raster
             if FileType=='Image file':
@@ -91,17 +87,6 @@ for line in ControlTable.values: # loop through each landscape_var in control ta
                 # for now we assume multiplication of values but may modify this in future
                 rasterMath(inras, tempras, expression= inras + ' * ' + str(ModifyBy), out_dtype=RastType)
         
-            # get extents from template to pass to gdalwarp
-            NDV, Stats, xsize, ysize, GeoT, Proj_projcs, Proj_geogcs, DataType = getRasterInfo(Template)
-            minx = GeoT[0]
-            maxy = GeoT[3]
-            maxx = minx + GeoT[1]*xsize
-            miny = maxy + GeoT[5]*ysize
-            bounds = str(minx) + " " + str(miny) + " " + str(maxx) + " " + str(maxy)
-            template_projection = Proj_projcs
-            dst_xsize = xsize
-            dst_ysize = ysize
-            dst_crs = 'EPSG:5070'
             
             # if temp raster hasn't been created in previous steps, just poiint to input raster
             if not os.path.isfile(TempDir + '/' + Rast + '.tif') or not os.path.isfile(TempDir + '/' + Rast + '_2.tif'):
@@ -114,47 +99,48 @@ for line in ControlTable.values: # loop through each landscape_var in control ta
             
             # get raster info from temp raster
             NDV, Stats, xsize, ysize, GeoT, Proj_projcs, Proj_geogcs, DataType = getRasterInfo(tempras)
+            minx = GeoT[0]
+            maxy = GeoT[3]
+            maxx = minx + GeoT[1]*xsize
+            miny = maxy + GeoT[5]*ysize
+            bounds = str(minx) + " " + str(miny) + " " + str(maxx) + " " + str(maxy)
             outDataType = DataType
             outNDV = NDV
+            dst_crs = 'EPSG:5070'
             
             # If final file doesn't already exists use gdal resample to resample to desired resolution and project.  
             # Also apply a mask if needed to produce final raster
             if not os.path.isfile(FinalDir + '/' + Rast + '.tif'):
-#                 Set Geoprocessing environments
-                arcpy.env.snapRaster = '%s'%(Template)
-                arcpy.env.cellSize = '%s'%(ConvertRes)
-#                arcpy.env.mask = "L:\\Priv\\CORFiles\\Geospatial_Library\\Data\\RESOURCE\\POLITICAL\\BOUNDARIES\\NATIONAL\\Census_2000_State_State_Equivalents_Cartographic_Boundaries.shp"
-                # Process: Project Raster               
-                finalras = FinalDir + '/' + Rast + '.tif'
-                arcpy.ProjectRaster_management(tempras, finalras, "PROJCS['NAD_1983_Contiguous_USA_Albers',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Albers'],PARAMETER['false_easting',0.0],PARAMETER['false_northing',0.0],PARAMETER['central_meridian',-96.0],PARAMETER['standard_parallel_1',29.5],PARAMETER['standard_parallel_2',45.5],PARAMETER['latitude_of_origin',23.0],UNIT['Meter',1.0]]", "NEAREST", "1000 1000", "", "", "PROJCS['NAD_1983_Albers',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Albers'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['central_meridian',-96.0],PARAMETER['Standard_Parallel_1',29.5],PARAMETER['Standard_Parallel_2',45.5],PARAMETER['latitude_of_origin',23.0],UNIT['Meter',1.0]]")
-
-                if not Proj_projcs==dst_crs and not type(MaskRas) is str:
-                    resamp_ras = FinalDir + '/' + Rast + '.tif'
-                    resamp_string = "gdalwarp --config GDAL_DATA " + '"C:/Users/mweber/AppData/Local/Continuum/Anaconda/pkgs/libgdal-1.11.2-2/Library/data" ' +' -tr ' + str(ConvertRes) + ' -' + str(ConvertRes) + " -te " + bounds + " -srcnodata " + str(outNDV) +  " -dstnodata "  + str(outNDV) +  " -of GTiff -r near -t_srs " + dst_crs + " -co COMPRESS=DEFLATE -co TFW=YES -co TILED=YES -co TIFF_USE_OVR=TRUE -ot " + outDataType + " " + tempras + " " + resamp_ras
-                    startTime = dt.now()
-                    call(resamp_string)
-                    print "elapsed time " + str(dt.now()-startTime)
-                    
-                if not Proj_projcs==dst_crs and type(MaskRas) is str:
-                    resamp_ras = TempDir + '/' + Rast + 'Resmp.tif'
-                    resamp_string = "gdalwarp --config GDAL_DATA " + '"C:/Users/mweber/AppData/Local/Continuum/Anaconda/pkgs/libgdal-1.11.2-2/Library/data" ' +' -tr ' + str(ConvertRes) + ' -' + str(ConvertRes) + " -te " + bounds + " -srcnodata " + str(outNDV) +  " -dstnodata "  + str(outNDV) +  " -of GTiff -r near -t_srs " + dst_crs + " -co COMPRESS=DEFLATE -co TFW=YES -co TILED=YES -co TIFF_USE_OVR=TRUE -ot " + outDataType + " " + tempras + " " + resamp_ras
-                    startTime = dt.now()
-                    call(resamp_string)
-                    print "elapsed time " + str(dt.now()-startTime)
-
-                    # Execute ExtractByMask
-                    final_ras = FinalDir + '/' + Rast + '.tif'
-                    import arcpy
-                    from arcpy.sa import *
-                    arcpy.CheckOutExtension("Spatial")
-                    arcpy.gp.ExtractByMask_sa(resamp_ras, MaskRas, final_ras)
-                    outExtractByMask = ExtractByMask(resamp_ras, MaskRas)
-#                     Save the output                    
-                    outExtractByMask.save('L:/Priv/CORFiles/Geospatial_Library/Data/Project/SSWR1.1B/LandscapeRasters/QAComplete/roadden.tif')
-            # examine results
-            NDV, Stats, xsize, ysize, GeoT, Proj_projcs, Proj_geogcs, DataType = getRasterInfo(resamp_ras)
-#            gdalwarp -overwrite -s_srs EPSG:5070 -q -cutline L:/Priv/CORFiles/Geospatial_Library/Data/Project/SSWR1.1B/LandscapeRasters/QAComplete/water_mask.tif -dstalpha -of GTiff C:/Users/mweber/Temp/roadden.tif C:/Users/mweber/Temp/test2.tif                
-#        gdalwarp -q -cutline L:/Priv/CORFiles/Geospatial_Library/Data/RESOURCE/POLITICAL/BOUNDARIES/NATIONAL/Census_2000_State_State_Equivalents_Cartographic_Boundaries.shp -crop_to_cutline -of GTiff L:/Priv/CORFiles/Geospatial_Library/Data/Project/StreamCat/LandscapeRasters/QAComplete/roadden.tif C:/Users/mweber/Temp/roadden_3.tif
+               if UseArcpy=='Yes': 
+                   if UseStatesMask=='No':
+                       # Project to temporary raster      
+                       finalras = FinalDir + '/' + Rast + '.tif'
+                       arcpy.ProjectRaster_management(tempras, finalras, "PROJCS['NAD_1983_Contiguous_USA_Albers',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Albers'],PARAMETER['false_easting',0.0],PARAMETER['false_northing',0.0],PARAMETER['central_meridian',-96.0],PARAMETER['standard_parallel_1',29.5],PARAMETER['standard_parallel_2',45.5],PARAMETER['latitude_of_origin',23.0],UNIT['Meter',1.0]]", "NEAREST", "1000 1000", "", "", "PROJCS['NAD_1983_Albers',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Albers'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['central_meridian',-96.0],PARAMETER['Standard_Parallel_1',29.5],PARAMETER['Standard_Parallel_2',45.5],PARAMETER['latitude_of_origin',23.0],UNIT['Meter',1.0]]")
+                   if UseStatesMask=='Yes':
+                        # Project to temporary raster      
+                        projras = TempDir + '/' + Rast + 'proj.tif'
+                        # Execute ExtractByMask
+                        MaskRas = 'L:/Priv/CORFiles/Geospatial_Library/Data/RESOURCE/POLITICAL/BOUNDARIES/NATIONAL/States_limited_borders.shp'
+                        from arcpy.sa import *
+                        arcpy.CheckOutExtension("Spatial")
+                        outExtractByMask = ExtractByMask(projras, MaskRas)
+        #               Save the output      
+                        finalras = FinalDir + '/' + Rast + '.tif'
+                        outExtractByMask.save(finalras)
+               if UseArcpy=='No':
+                   if not Proj_projcs==dst_crs and not type(MaskRas) is str:
+                        resamp_ras = FinalDir + '/' + Rast + '.tif'
+                        resamp_string = "gdalwarp --config GDAL_DATA " + '"C:/Users/mweber/AppData/Local/Continuum/Anaconda/pkgs/libgdal-1.11.2-2/Library/data" ' +' -tr ' + str(ConvertRes) + ' -' + str(ConvertRes) + " -te " + bounds + " -srcnodata " + str(outNDV) +  " -dstnodata "  + str(outNDV) +  " -of GTiff -r near -t_srs " + dst_crs + " -co COMPRESS=DEFLATE -co TFW=YES -co TILED=YES -co TIFF_USE_OVR=TRUE -ot " + outDataType + " " + tempras + " " + resamp_ras
+                        startTime = dt.now()
+                        call(resamp_string)
+                        print "elapsed time " + str(dt.now()-startTime)
+                        
+                   if not Proj_projcs==dst_crs and type(MaskRas) is str:
+                        resamp_ras = TempDir + '/' + Rast + 'Resmp.tif'
+                        resamp_string = "gdalwarp --config GDAL_DATA " + '"C:/Users/mweber/AppData/Local/Continuum/Anaconda/pkgs/libgdal-1.11.2-2/Library/data" ' +' -tr ' + str(ConvertRes) + ' -' + str(ConvertRes) + " -te " + bounds + " -srcnodata " + str(outNDV) +  " -dstnodata "  + str(outNDV) +  " -of GTiff -r near -t_srs " + dst_crs + " -co COMPRESS=DEFLATE -co TFW=YES -co TILED=YES -co TIFF_USE_OVR=TRUE -ot " + outDataType + " " + tempras + " " + resamp_ras
+                        startTime = dt.now()
+                        call(resamp_string)
+                        print "elapsed time " + str(dt.now()-startTime)
         # Processes for vector features
         if FileType== 'ESRI Shapefile':
             Feat = gpd.GeoDataFrame.from_file(InDir + '/' + Rast + '.shp')
