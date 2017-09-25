40ba7111c37f5fd9f8833c483064e2720b905978
Marc Weber <mweber36@gmail.com>
2017-09-25 12:25:22 -0700

    minor commit

--- FinalTablesStates.py
+++ FinalTablesStates.py
@@ -7,7 +7,7 @@ FinalTablesStates
 import pandas as pd
 import os
 
-var = 'ImperviousSurfaces2011RipBuf100'
+var = 'NRSA_PredictedBioCondition'
 # Read in a state / hydro-region lookup table
 lookupdir = 'L:/Priv/CORFiles/Geospatial_Library/Data/Project/StreamCat/StateLookup'
 stateVPU = pd.read_csv(lookupdir + '/State_VPU.csv')
