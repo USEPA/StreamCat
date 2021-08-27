3ae0abfa704341b1dc4064455f88494db3d1ab8a
Rick Debbout <debboutr@gmail.com>
2021-08-28 05:10:56 +1100

    Catch KeyError when metric already processed (#91)
    * identify existence of accumulated files, warn and exit
    
    * run through all control metrics before printing problems
    
    * uppercase config imports
    
    Co-authored-by: Debbout <Debbout.Rick@epa.gov>

--- StreamCat.py
+++ StreamCat.py
@@ -24,123 +24,115 @@
  > python StreamCat.py
  --------------------------------------------------------
 """
-# TODO: remake npz files for bastards only, don't retain comids w/o upstream!
-# TODO: calculate WS metrics by table math, not in numpy accumulation process
-# TODO: create function like findUpstreamNPY for up and ws
 
 import os
-from datetime import datetime as dt
 
+import click
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+from stream_cat_config import (LYR_DIR, MASK_DIR_RP100, MASK_DIR_SLP10,
+                               MASK_DIR_SLP20, NHD_DIR, OUT_DIR, PCT_FULL_FILE,
+                               PCT_FULL_FILE_RP100)
 
-from stream_cat_config import (
-    LYR_DIR,
-    MASK_DIR_RP100,
-    MASK_DIR_SLP10,
-    MASK_DIR_SLP20,
-    NHD_DIR,
-    OUT_DIR,
-    PCT_FULL_FILE,
-    PCT_FULL_FILE_RP100,
-)
-from StreamCat_functions import (
-    Accumulation,
-    PointInPoly,
-    appendConnectors,
-    createCatStats,
-    interVPU,
-    makeNumpyVectors,
-    nhd_dict,
-)
-
-# Load table of layers to be run...
-ctl = pd.read_csv("ControlTable_StreamCat.csv")
+from StreamCat_functions import (Accumulation, PointInPoly, appendConnectors,
+                                 createCatStats, interVPU, makeNumpyVectors,
+                                 nhd_dict)
 
 
-totTime = dt.now()
+@click.command()
+def run_stream_cat():
+    """Process Landscape Layers through NHDPlusV21 framework"""
 
-# Load table of inter vpu connections
-inter_vpu = pd.read_csv("InterVPU.csv")
+    # Load table of layers to be run...
+    ctl = pd.read_csv("ControlTable_StreamCat.csv")
 
-if not os.path.exists(OUT_DIR):
-    os.mkdir(OUT_DIR)
+    # Load table of inter vpu connections
+    inter_vpu = pd.read_csv("InterVPU.csv")
 
-if not os.path.exists("accum_npy"):
-    # TODO: work out children OR bastards only
-    makeNumpyVectors(inter_vpu, NHD_DIR)
+    if not os.path.exists(OUT_DIR):
+        os.mkdir(OUT_DIR)
 
-INPUTS = np.load("accum_npy/vpu_inputs.npy", allow_pickle=True).item()
+    if not os.path.exists("accum_npy"):
+        # TODO: work out children OR bastards only
+        makeNumpyVectors(inter_vpu, NHD_DIR)
 
-for _, row in ctl.query("run == 1").iterrows():
+    INPUTS = np.load("accum_npy/vpu_inputs.npy", allow_pickle=True).item()
 
-    print("running: " + row.FullTableName)
-    apm = "" if row.AppendMetric == "none" else row.AppendMetric
-    if row.use_mask == 1:
-        mask_dir = MASK_DIR_RP100
-    elif row.use_mask == 2:
-        mask_dir = MASK_DIR_SLP10
-    elif row.use_mask == 3:
-        mask_dir = MASK_DIR_SLP20
-    else:
-        mask_dir = ""
-    LL = f"{LYR_DIR}/{row.LandscapeLayer}"
-    summaryfield = None
-    if type(row.summaryfield) == str:
-        summaryfield = row.summaryfield.split(";")
-    if row.accum_type == "Point":  # Load in point geopandas table and Pct_Full table
-        if row.use_mask == 0:  # TODO: script to create this pct_full_file
-            pct_full_file = PCT_FULL_FILE
+    already_processed = []
+
+    for _, row in ctl.query("run == 1").iterrows():
+
+        apm = "" if row.AppendMetric == "none" else row.AppendMetric
         if row.use_mask == 1:
-            pct_full_file = PCT_FULL_FILE_RP100
-        pct_full = pd.read_csv(pct_full_file)
-        points = gpd.GeoDataFrame.from_file(LL)
-    if not os.path.exists(OUT_DIR + "/DBF_stash"):
-        os.mkdir(OUT_DIR + "/DBF_stash")
-    # File string to store InterVPUs needed for adjustments
-    Connector = f"{OUT_DIR}/{row.FullTableName}_connectors.csv"
-    catTime = dt.now()
-    for zone, hydroregion in INPUTS.items():
-        if not os.path.exists(f"{OUT_DIR}/{row.FullTableName}_{zone}.csv"):
-            pre = f"{NHD_DIR}/NHDPlus{hydroregion}/NHDPlus{zone}"
-            if not row.accum_type == "Point":
-                izd = (
-                    f"{mask_dir}/{zone}.tif"
-                    if mask_dir
-                    else f"{pre}/NHDPlusCatchment/cat"
-                )
-                cat = createCatStats(
-                    row.accum_type,
-                    LL,
-                    izd,
-                    OUT_DIR,
-                    zone,
-                    row.by_RPU,
-                    mask_dir,
-                    NHD_DIR,
-                    hydroregion,
-                    apm,
-                )
-            if row.accum_type == "Point":
-                izd = f"{pre}/NHDPlusCatchment/Catchment.shp"
-                cat = PointInPoly(
-                    points, zone, izd, pct_full, mask_dir, apm, summaryfield
-                )
-            cat.to_csv(f"{OUT_DIR}/{row.FullTableName}_{zone}.csv", index=False)
-            in2accum = len(cat.columns)
-    print("Cat Results Complete in : " + str(dt.now() - catTime))
-    try:
-        # if in2accum not defined...Cat process done,but error thrown in accum
-        in2accum
-    except NameError:
-        # get number of columns to test if accumulation needs to happen
-        in2accum = len(pd.read_csv(f"{OUT_DIR}/{row.FullTableName}_{zone}.csv").columns)
-    accumTime = dt.now()
-    for zone in INPUTS:
-        cat = pd.read_csv(f"{OUT_DIR}/{row.FullTableName}_{zone}.csv")
-        in2accum = len(cat.columns)
-        if len(cat.columns) == in2accum:
+            mask_dir = MASK_DIR_RP100
+        elif row.use_mask == 2:
+            mask_dir = MASK_DIR_SLP10
+        elif row.use_mask == 3:
+            mask_dir = MASK_DIR_SLP20
+        else:
+            mask_dir = ""
+        LL = f"{LYR_DIR}/{row.LandscapeLayer}"
+        summaryfield = None
+        if type(row.summaryfield) == str:
+            summaryfield = row.summaryfield.split(";")
+        if (
+            row.accum_type == "Point"
+        ):  # Load in point geopandas table and Pct_Full table
+            # TODO: script to create this PCT_FULL_FILE
+            pct_full = pd.read_csv(
+                PCT_FULL_FILE if row.use_mask == 0 else PCT_FULL_FILE_RP100
+            )
+            points = gpd.read_file(LL)
+        if not os.path.exists(OUT_DIR + "/DBF_stash"):
+            os.mkdir(OUT_DIR + "/DBF_stash")
+        # File string to store InterVPUs needed for adjustments
+        Connector = f"{OUT_DIR}/{row.FullTableName}_connectors.csv"
+        print(
+            f"Acquiring `{row.FullTableName}` catchment statistics...",
+            end="",
+            flush=True,
+        )
+        for zone, hydroregion in INPUTS.items():
+            if not os.path.exists(f"{OUT_DIR}/{row.FullTableName}_{zone}.csv"):
+                print(zone, end=", ", flush=False)
+                pre = f"{NHD_DIR}/NHDPlus{hydroregion}/NHDPlus{zone}"
+                if not row.accum_type == "Point":
+                    izd = (
+                        f"{mask_dir}/{zone}.tif"
+                        if mask_dir
+                        else f"{pre}/NHDPlusCatchment/cat"
+                    )
+                    cat = createCatStats(
+                        row.accum_type,
+                        LL,
+                        izd,
+                        OUT_DIR,
+                        zone,
+                        row.by_RPU,
+                        mask_dir,
+                        NHD_DIR,
+                        hydroregion,
+                        apm,
+                    )
+                if row.accum_type == "Point":
+                    izd = f"{pre}/NHDPlusCatchment/Catchment.shp"
+                    cat = PointInPoly(
+                        points, zone, izd, pct_full, mask_dir, apm, summaryfield
+                    )
+                cat.to_csv(f"{OUT_DIR}/{row.FullTableName}_{zone}.csv", index=False)
+        print("done!")
+        print("Accumulating...", end="", flush=True)
+        for zone in INPUTS:
+            fn = f"{OUT_DIR}/{row.FullTableName}_{zone}.csv"
+            cat = pd.read_csv(fn)
+            processed = cat.columns.str.extract(r"^(UpCat|Ws)").any().bool()
+            if processed:
+                print("skipping!", flush=False)
+                already_processed.append(row.FullTableName)
+                break
+            print(zone, end=", ", flush=True)
+
             if zone in inter_vpu.ToZone.values:
                 cat = appendConnectors(cat, Connector, zone, inter_vpu)
             accum = np.load(f"accum_npy/accum_{zone}.npz")
@@ -172,5 +164,16 @@ for _, row in ctl.query("run == 1").iterrows():
             upFinal = pd.merge(up, ws, on="COMID")
             final = pd.merge(cat, upFinal, on="COMID")
             final.to_csv(f"{OUT_DIR}/{row.FullTableName}_{zone}.csv", index=False)
-    print("Accumulation Results Complete in : " + str(dt.now() - accumTime))
-print("total elapsed time " + str(dt.now() - totTime))
+        print(end="") if processed else print("done!")
+    if already_processed:
+        print(
+            "\n!!!Processing Problem!!!\n\n"
+            f"{', '.join(already_processed)} already run!\n"
+            "Be sure to delete the associated files in your `OUTDIR` to rerun:"
+            f"\n\t> {OUT_DIR}\n\n!!! `$OUT_DIR/DBF_stash/*` "
+            f"output used in 'Continuous' and 'Categorical' metrics!!!"
+        )
+
+
+if __name__ == "__main__":
+    run_stream_cat()
