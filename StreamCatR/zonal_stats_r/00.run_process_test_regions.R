a9226a3ca96d1da957367abec1add5fb7ded5db0
Ryan Hill <hill.ryan@epa.gov>
2026-02-09 17:04:56 -0800

    refactoring for github

--- StreamCatR/zonal_stats_r/00.run_process_test_regions.R
+++ StreamCatR/zonal_stats_r/00.run_process_test_regions.R
@@ -11,19 +11,19 @@ Sys.setenv(GDAL_NUM_THREADS = "ALL_CPUS")
 Sys.setenv(OMP_NUM_THREADS  = as.character(parallel::detectCores()))
 terraOptions(threads = parallel::detectCores(), memfrac = 0.9)
 
-region_id <- "Region16"
+region_id <- "Region17"
 
 #-------------------------------------------------------
 # Prepare region catchment raster
 #-------------------------------------------------------
-source("01.build_optimized_catchments.R")
-catchment_path   <- "C:/Users/RHill04/WorkFolder/GIS/NHDPlusV21/NHDPlusGB/NHDPlus16/NHDPlusCatchment/cat"
+source("./StreamCatR/zonal_stats_r/01.build_optimized_catchments.R")
+catchment_path   <- "C:/Users/RHill04/WorkFolder/GIS/NHDPlusV21/NHDPlusPN/NHDPlus17/NHDPlusCatchment/cat"
 tic()
 prepare_optimized_catchments(
   catchment_path   = catchment_path,
   region_id        = region_id,
   blocksize        = 6144L,
-  outdir           = "./cat_rasters",
+  outdir           = "./StreamCatR/zonal_stats_r/cat_rasters",
   target_crs       = "EPSG:5070",
   overwrite_rasters = TRUE,
   build_windows     = TRUE,  
@@ -34,15 +34,15 @@ toc()
 #-------------------------------------------------------
 # Optimize continuous raster (block size)
 #-------------------------------------------------------
-source("02.optimize_continuous_raster.R")
+source("./StreamCatR/zonal_stats_r/02.optimize_continuous_raster.R")
 
-indir <- "./landscape_rasters/"
+indir <- "./StreamCatR/zonal_stats_r/landscape_rasters/"
 #blocksize = "512"
 inras <- "bfi"
 
 optimize_continuous_raster(
-  infile     = file.path("./landscape_rasters", "bfi.tif"),
-  outdir     = "./landscape_rasters",
+  infile     = file.path("./StreamCatR/zonal_stats_r/landscape_rasters", "bfi.tif"),
+  outdir     = "./StreamCatR/zonal_stats_r/landscape_rasters",
   target_crs = "EPSG:5070",     # skip or set NULL if no reprojection needed
   resample   = "bilinear",      # keep for continuous data
   datatype   = "FLT4S"          # float for continuous rasters
@@ -51,22 +51,22 @@ optimize_continuous_raster(
 #-------------------------------------------------------
 # Run zonal
 #-------------------------------------------------------
-source("04.zonal_functions_true_blocking.R")
-source("05.run_region_accumulation.R")
+source("./StreamCatR/zonal_stats_r/04.zonal_functions_true_blocking.R")
+source("./StreamCatR/zonal_stats_r/05.run_region_accumulation.R")
 
 # Thread options (optional)
 Sys.setenv(GDAL_NUM_THREADS = "ALL_CPUS")
 Sys.setenv(OMP_NUM_THREADS  = as.character(parallel::detectCores()))
 if (!dir.exists("./terra")) dir.create("./terra", recursive = TRUE, showWarnings = FALSE)
 terra::terraOptions(threads = parallel::detectCores(),
-                    tempdir = "./terra",
+                    tempdir = "./StreamCatR/zonal_stats_r/terra",
                     memfrac = 0.9)
 
 tictoc::tic()
 dt2 <- run_region_accumulation(
   region_id = region_id,
-  zone_dir = "./cat_rasters",                 # folder with Zidx and parquet files
-  predictor_path = "./landscape_rasters/bfi_512.tif",           # full path to the predictor raster (any tiling/size)
+  zone_dir = "./StreamCatR/zonal_stats_r/cat_rasters",                 # folder with Zidx and parquet files
+  predictor_path = "./StreamCatR/zonal_stats_r/landscape_rasters/bfi_512.tif",           # full path to the predictor raster (any tiling/size)
   blocksize = 6144,                # e.g., 6144L (used only for Zidx/windows)
   method = "near",
   stats  = "sum",
